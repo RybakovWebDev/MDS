dac0729114aa352d121e91bddeb4f05a849f97d9
AR <rybakovwebdev@gmail.com>
2023-08-31 23:14:32 +0400

    Update backend code for CORS

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,20 @@ const Watchlist = require("./models/watchlistModel");
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 app.use(express.json());
 
 app.use(express.json());
